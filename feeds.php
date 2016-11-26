383426dd4f890a3ced14a1fe4b9a833a35ae7687
Surfing-Chef <surfing-chef@surfing-chef.com>
2016-11-26 11:43:41 -0800

    Returned $latest_posts as array container

--- feeds.php
+++ feeds.php
@@ -73,92 +73,39 @@ function latest_posts ($https, $json='' ){
     $count = 0;
 
     while($count < $toget) {
-      $container = array();
-        // $num_headings = 0;
-        // find the headings
-        $headings = $html->find('a[class=topic-index-topic-label]');
-        // populate $posts_array with the selected headings
-        foreach($headings as $heading){
-          //echo $heading->plaintext.'</br>';
-          $container[$site]['heading'][] = $heading->plaintext;
-        }
+      // $num_headings = 0;
+      // find the headings
+      $headings = $html->find('a[class=topic-index-topic-label]');
+      // populate $posts_array with the selected headings
+      foreach($headings as $heading){
+        //echo $heading->plaintext.'</br>';
+        $posts_array[$site]['heading'][] = $heading->plaintext;
+      }
 
-          // find the descriptions
-          $descriptions = $html->find('div[class=topic-index-tile] > h3 > a');
-          foreach($descriptions as $description){
-              //echo $description->plaintext.'</br>';
-              $container[$site]['description'][]= $description->plaintext;
-          }
-
-          // find the images
-          $media = "data-pin-media";
-          $images = $html->find('div[class=topic-index-tile] > a img');
-          foreach($images as $image){
-              $container[$site]['image'][] = $image->$media;
-          }
-
-          // find the links
-          $urls = $html->find('div[class=topic-index-tile] > h3 > a');
-          foreach($urls as $url){
-            $container[$site]['link'][] = $url_short.$url->href;
-          }
-        $count ++;
-        }
-      //}
-    //}
+      // // find the descriptions
+      // $descriptions = $html->find('div[class=topic-index-tile] > h3 > a');
+      // foreach($descriptions as $description){
+      //     //echo $description->plaintext.'</br>';
+      //     $container[$site]['description'][]= $description->plaintext;
+      // }
+      //
+      // // find the images
+      // $media = "data-pin-media";
+      // $images = $html->find('div[class=topic-index-tile] > a img');
+      // foreach($images as $image){
+      //     $container[$site]['image'][] = $image->$media;
+      // }
+      //
+      // // find the links
+      // $urls = $html->find('div[class=topic-index-tile] > h3 > a');
+      // foreach($urls as $url){
+      //   $container[$site]['link'][] = $url_short.$url->href;
+      // }
+    $count ++;
+    }
   }
-  // if ($https[0]['name'] == 'Food52' || 'Food 52') {
-  //   // POPULATE POST_ARRAY
-  //   // create a loop $num_post times to populate $post_array
-  //   $count = 0;
-  //
-  //   while($count < $toget) {
-  //
-  //     if ($count < 1){
-  //       // Target header
-  //       $heading_container = $html->find("div.topic-page-body h2 a");
-  //       $heading = $heading_container[$count]->plaintext;
-  //       echo $heading.'</br>';
-  //       $posts_array[$site][$count]['heading'] = $heading;
-  //       // Target image
-  //       $media = 'data-pin-media';
-  //       $image_container = $html->find("div.topic-page-body div.topic-index-group div.topic-index-tile a img");
-  //       $image = $image_container[$count]->$media;
-  //       echo $image.'</br>';
-  //       $posts_array[$site][$count]['image'] = $image;
-  //       // Target description
-  //       $description_container = $html->find("div.topic-page-body div.topic-index-group div.topic-index-tile h3");
-  //       $description = $description_container[$count]->plaintext;
-  //       echo $description.'</br>';
-  //       $posts_array[$site][$count]['description'] = $description;
-  //       // Target link
-  //       $link_container = $html->find("div.topic-page-body div.topic-index-group div.topic-index-tile h3 a");
-  //       $link = $url_short.$link_container[$count]->href;
-  //       echo $link.'</br>'.'</br>';
-  //       $posts_array[$site][$count]['link'] = $link;
-  //       $count ++;
-  //     }
-  //     if ($count > 0){
-  //       $cont = "div.topic-page-wrapper:nth-child(3) > div > ";
-  //       // Target header
-  //       $heading_container = $html->find($cont."h2 a.topic-index-topic-label");
-  //       $heading = $heading_container[$count]->plaintext;
-  //       echo $heading.'</br>';
-  //       $posts_array[$site][$count]['heading'] = $heading;
-  //       // Target description
-  //
-  //       $description_container = $html->find('.block-container > div:nth-child(3) .topic-index-title div div div a > h3 > a');
-  //       $description = $description_container[0]->plaintext;
-  //       echo $description.'</br>';
-  //
-  //       $posts_array[$site][$count]['description'] = $description;
-  //
-  //       $count ++;
-  //     }
-  //   }
-  // }
   // // returns JSON data
-  echo json_encode($container);
+  //echo json_encode($latest_posts);
 } // end function latest_posts()
 
 // CALL function
